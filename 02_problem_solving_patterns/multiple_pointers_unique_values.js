da55a3d27f5fbf50d418da82a85c77f6d3c111f1
unknown <riskeconomics@gmail.com>
2022-09-30 13:07:15 -0400

    commit patron contador de valor unico

--- 02_problem_solving_patterns/multiple_pointers_unique_values.js
+++ 02_problem_solving_patterns/multiple_pointers_unique_values.js
@@ -1,2 +1,32 @@
 //multiple pointers: count unique value challenge
 //punteros múltiples: contar el desafío del valor único
+let countUniqueValue = (arr) => {
+  let unique = new Set(arr);
+  return unique.size;
+};
+let arr = [1, 1, 1, 2, 3, 3, 4, 4, 5, 6];
+let result = countUniqueValue(arr);
+console.log(result);
+
+function contadorValoresUnicos(arr) {
+  if (!typeof arr === Array.isArray(arr)) {
+    return 0;
+  }
+  if (arr.length === 0) {
+    return 0;
+  }
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+    console.log(i, j);
+  }
+  return i + 1;
+}
+let arr1 = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3]; //3
+let arr2 = []; //0
+let arr3 = false; // 0
+let resultado = contadorValoresUnicos(arr3);
+console.log(resultado);
